446a54635f50c45d66d80928f8a8ed521d789e72
ezzoubair <ezzoubair@pop-os.localdomain>
2024-01-19 02:00:43 +0100

    first commit

---
+++ src/lib/Widgets/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import  {AppBar} from "@skeletonlabs/skeleton"
+</script>
+
+
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<img class="h-12" src="/icon.png" alt="" srcset="">
+	</svelte:fragment>
+	<h1 class="h3">H1roune</h1>
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { Header } from '$lib';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<Header />
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>

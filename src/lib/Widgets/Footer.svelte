47756ee5a1472a37e806ca951b5552cc3db4b212
ezzoubair <ezzoubair@pop-os.localdomain>
2024-01-21 16:35:41 +0100

    create a footer. add logo cloud. add icons. links after finishing.

--- src/lib/Widgets/Footer.svelte
+++ src/lib/Widgets/Footer.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { InstagramIcon, WhatsappIcon, XIcon, BehanceIcon } from '$lib';
 </script>
 
 <section class="w-full">
 	<section class="logo-cloud gap-1 lg:grid lg:grid-cols-2 xl:flex xl:items-center xl:content-center w-full px-4 lg:px-20  pb-2">
-		<a class="logo-item rounded-md" href="/">
+		<a class="logo-item rounded-md" href="{base}/">
 			<span><InstagramIcon /></span>
 			<span>Instagram</span>
 		</a>
-		<a class="logo-item rounded-md" href="/">
+		<a class="logo-item rounded-md" href="{base}/">
 			<span><WhatsappIcon /></span>
 			<span>Whatsapp</span>
 		</a>
-		<a class="logo-item rounded-md" href="/">
+		<a class="logo-item rounded-md" href="{base}/">
 			<span><XIcon /></span>
 			<span>Twitter</span>
 		</a>
-		<a class="logo-item rounded-md" href="/">
+		<a class="logo-item rounded-md" href="{base}/">
 			<span><BehanceIcon /></span>
 			<span>Behance</span>
 		</a>
